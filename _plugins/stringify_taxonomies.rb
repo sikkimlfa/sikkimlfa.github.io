1bbb1b8a9660945ba5a2cbcdda88bc599e46c161
sikkimlfa <sikkimlfa@gmail.com>
2026-08-07 21:33:54 +0530

    Delete _plugins/stringify_taxonomies.rb

--- _plugins/stringify_taxonomies.rb
+++
@@ -1,9 +0,0 @@
-Jekyll::Hooks.register :posts, :pre_render do |post|
-  if post.data['categories'].is_a?(Array)
-    post.data['categories'] = post.data['categories'].map(&:to_s)
-  end
-
-  if post.data['tags'].is_a?(Array)
-    post.data['tags'] = post.data['tags'].map(&:to_s)
-  end
-end
